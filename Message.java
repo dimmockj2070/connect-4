65d023b19b88c7936f5b615d22cdbf9db348bb83
Joel <jbdimmock@gmail.com>
2023-05-20 13:28:28 -0700

    Didn't get pushed last time for some reason

---
+++ Message.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private String type;
+    private int move;
+
+    public Message(String type, int move) {
+        this.type = type;
+        this.move = move;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getMove() {
+        return move;
+    }
+}
